0ea409ac7226853362a6bb680323c888d3cee6cf
Larry Davis <lazd@lazd.net>
2013-01-19 11:05:08 -0800

    Use preloaded models & materials, center ground around 0,0,0

--- client/duneBuggy/classes/db.Ground.js
+++ client/duneBuggy/classes/db.Ground.js
@@ -15,41 +15,38 @@
 	
 			this.groundMeshes = [];
 	
+			var geometry = this.groundGeometry = this.game.models.terrain.geometry;
+			var materials = this.groundGeometry = this.game.models.terrain.materials;
+	
+			// TODO: apply materials
+			materials.forEach(function(material, index) {
+				material.map.wrapS = THREE.RepeatWrapping;
+				material.map.wrapT = THREE.RepeatWrapping;
+				material.map.repeat.set(0.125, 0.125);
+				
+				materials[index] = Physijs.createMaterial(
+					material,
+					1, // high friction
+					.4 // low restitution
+				);
+			});
 			
-			// Load model
-			var loader = new THREE.JSONLoader();
-			loader.load("duneBuggy/models/terrain.js", function(geometry, materials) {
-				this.groundGeometry = geometry;
-		
-				// TODO: apply materials
-				materials.forEach(function(material, index) {
-					material.map.wrapS = THREE.RepeatWrapping;
-					material.map.wrapT = THREE.RepeatWrapping;
-					material.map.repeat.set(0.125, 0.125);
-					
-					materials[index] = Physijs.createMaterial(
-						material,
-						1, // high friction
-						.4 // low restitution
-					);
-				});
-				
-				this.groundGeometry = geometry;
-				this.groundMaterials = materials;
-				
-				var xSpacingMax = 2326.48;
-				var xSpacingMin = -2326.48;
-				var zSpacingMax = 2339.54;
-				var zSpacingMin = -2419.3;
-				var yHeight = 20;
-				
-				//this.groundMesh = new THREE.Mesh(geometry, faceMaterial);
-				this.makeGround(new THREE.Vector3(xSpacingMax/2,yHeight,zSpacingMax/2), new THREE.Vector3(0,Math.PI/4,0));
-				//this.makeGround(new THREE.Vector3(xSpacingMax,yHeight,xSpacingMax), new THREE.Vector3(0,0,0));
-				//this.makeGround(new THREE.Vector3(xSpacingMax,yHeight,zSpacingMin-xSpacingMax+zSpacingMin), new THREE.Vector3(0,-Math.PI,0));
-				//this.makeGround(new THREE.Vector3(xSpacingMin,yHeight,zSpacingMax), new THREE.Vector3(0,0,0));
-				//this.makeGround(new THREE.Vector3(xSpacingMin,yHeight,zSpacingMin), new THREE.Vector3(0,0,0));
-			}.bind(this));
+			this.groundGeometry = geometry;
+			this.groundMaterials = materials;
+			
+			var xSpacingMax = 2326.48;
+			var xSpacingMin = -2326.48;
+			var zSpacingMax = 2339.54;
+			var zSpacingMin = -2419.3;
+			var yHeight = 20;
+			
+			this.makeGround(new THREE.Vector3(0,0,0), new THREE.Vector3(0,0,0));
+			
+			// this.makeGround(new THREE.Vector3(xSpacingMax/2,yHeight,zSpacingMax/2), new THREE.Vector3(0,Math.PI/4,0));
+			//this.makeGround(new THREE.Vector3(xSpacingMax,yHeight,xSpacingMax), new THREE.Vector3(0,0,0));
+			//this.makeGround(new THREE.Vector3(xSpacingMax,yHeight,zSpacingMin-xSpacingMax+zSpacingMin), new THREE.Vector3(0,-Math.PI,0));
+			//this.makeGround(new THREE.Vector3(xSpacingMin,yHeight,zSpacingMax), new THREE.Vector3(0,0,0));
+			//this.makeGround(new THREE.Vector3(xSpacingMin,yHeight,zSpacingMin), new THREE.Vector3(0,0,0));
 			
 		},
 		makeGround: function(position, rotation) {
